b818645efb361ed03111057305cd036bccdea0f3
yutung-cheng <s1001913@gmail.com>
2023-09-09 14:13:44 -0400

    UI part done

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -17,6 +17,12 @@ export default function Home() {
           </div>
         </div>
       </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">New Songs</h1>
+        </div>
+        <div>List of Songs!</div>
+      </div>
     </div>
   );
 }
--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { FaPlay } from "react-icons/fa";
 import Image from "next/image";
 
 interface ListItemProps {
@@ -17,7 +18,10 @@ const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
     router.push(href);
   };
   return (
-    <button className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
+    <button
+      onClick={onClick}
+      className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4"
+    >
       <div className="relative min-h-[64px] min-w-[64px]">
         {/* This Image is imported from `next/Image` library. */}
         <Image
@@ -27,6 +31,10 @@ const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
           alt="Image"
           sizes="100%"
         />
+      </div>
+      <p className="font-medium truncate py-5">{name}</p>
+      <div className="absolute transition opacity-0 rounded-full flex items-center justify-center bg-green-500 p-3 drop-shadow-md right-5 group-hover:opacity-100 hover:scale-110">
+        <FaPlay className="text-black" />
       </div>
     </button>
   );
