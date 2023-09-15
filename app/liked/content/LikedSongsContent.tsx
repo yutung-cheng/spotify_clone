434555d70038e783b38695a643022e92474cb3e4
yutung-cheng <s1001913@gmail.com>
2023-09-15 16:30:10 -0400

    reformat

--- app/liked/content/LikedSongsContent.tsx
+++ app/liked/content/LikedSongsContent.tsx
@@ -33,8 +33,15 @@ const LikedSongContent: React.FC<LikedSongsContentProps> = ({
     }
     return (
       <div
-        className="px-6 grid 
-      grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8 gap-4 mt-4">
+        className="px-6
+        grid 
+        grid-cols-2 
+        sm:grid-cols-3 
+        md:grid-cols-3 
+        lg:grid-cols-4
+        xl:grid-cols-5
+        2xl:grid-cols-8
+        gap-4 mt-4">
         {songs.map((song) => {
           return (
             <SongItem key={song.id} onClick={() => {}} song={song} />
--- app/liked/page.tsx
+++ app/liked/page.tsx
@@ -9,11 +9,34 @@ const LikedSongs = async () => {
   const songs = await getLikedSongs();
 
   return (
-    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+    <div
+      className="bg-neutral-900
+    rounded-lg
+    h-full
+    w-full
+    overflow-hidden
+    overflow-y-auto">
       <Header>
         <div className="mt-10">
-          <div className="flex flex-row md:flex-row sm:flex-row items-center gap-x-5">
-            <div className=" bg-red-600 relative aspect-square h-16 w-16 lg:h-32 lg:w-32 md:h-24 md:w-24 sm:h-24 sm:w-24">
+          <div
+            className="flex
+          flex-row
+          md:flex-row
+          sm:flex-row
+          items-center
+          gap-x-5">
+            <div
+              className=" bg-red-600
+            relative
+            aspect-square
+            h-16
+            w-16
+            lg:h-32
+            lg:w-32
+            md:h-24
+            md:w-24
+            sm:h-24
+            sm:w-24">
               <Image
                 priority
                 fill
@@ -22,11 +45,25 @@ const LikedSongs = async () => {
                 sizes="100%"
               />
             </div>
-            <div className="flex flex-col gap-y-2 mt-4 md:mt-0">
-              <p className="hidden md:block font-semibold text-sm">
+            <div
+              className="flex
+            flex-col
+            gap-y-2
+            mt-4
+            md:mt-0">
+              <p
+                className="hidden
+              md:block
+              font-semibold
+              text-sm">
                 Playlist
               </p>
-              <h1 className="text-white text-3xl sm:text-4xl lg:text-6xl font-bold">
+              <h1
+                className="text-white
+              text-3xl
+              sm:text-4xl
+              lg:text-6xl
+              font-bold">
                 Liked Songs
               </h1>
             </div>
