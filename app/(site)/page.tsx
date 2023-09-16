8baeefcf5ef0f48c94c8033fcbc317007482eff3
yutung-cheng <s1001913@gmail.com>
2023-09-16 17:17:32 -0400

    ListItem songs play functionality completed.

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,12 +3,14 @@ import Header from "../components/Header";
 import ListItem from "../components/ListItem";
 import getSongs from "../actions/getSongs";
 import PageContent from "./content/PageContent";
+import getLikedSongs from "../actions/getLikedSongs";
 
 // This page will not be catch, and the data will always be up-to-date.
 export const revalidate = 0;
 
 export default async function Home() {
   const songs = await getSongs();
+  const likedSongs = await getLikedSongs();
 
   return (
     <div className="h-full w-full bg-neutral-900 rounded-lg overflow-hidden overflow-y-auto ">
@@ -19,6 +21,7 @@ export default async function Home() {
           </h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
             <ListItem
+              songs={likedSongs}
               name="Liked Songs"
               image="/images/liked.png"
               href="liked"
--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -7,28 +7,44 @@ import { useUser } from "../hooks/useUser";
 import useAuthModal from "../hooks/useAuthModal";
 import { useEffect, useState } from "react";
 import { useSessionContext } from "@supabase/auth-helpers-react";
+import { Song } from "@/types";
+import usePlayer from "../hooks/usePlayer";
 
 interface ListItemProps {
+  songs: Song[];
   image: string;
   name: string;
   href: string;
 }
 
-const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
+const ListItem: React.FC<ListItemProps> = ({
+  songs,
+  image,
+  name,
+  href,
+}) => {
   const router = useRouter();
   const { user } = useUser();
   const authModal = useAuthModal();
   const { session } = useSessionContext();
+  const player = usePlayer();
+
   const [isClicked, setIsClicked] = useState(false);
   const [logedIn, setLogedIn] = useState(false);
 
+  const handleClick = () => player.setId(songs[0].id);
+
   const onClick = () => {
     setIsClicked(true);
+    console.log("isClicked", isClicked);
+
     //Add authentication before push
     if (!user) {
       //If not log in, trigger log in dialog.
       authModal.onOpen();
     } else {
+      console.log("We are here 2");
+
       setIsClicked(false);
       router.push(href);
     }
@@ -47,22 +63,24 @@ const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
   }, [isClicked, session, logedIn, router, href]);
 
   return (
-    <button
-      onClick={onClick}
-      className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
-      <div className="relative min-h-[64px] min-w-[64px]">
-        {/* This Image is imported from `next/Image` library. */}
-        <Image
-          className="object-cover"
-          priority
-          fill
-          src={image}
-          alt="Image"
-          sizes="100%"
-        />
+    <button className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
+      <div onClick={onClick} className="flex flex-row">
+        <div className="relative min-h-[64px] min-w-[64px]">
+          {/* This Image is imported from `next/Image` library. */}
+          <Image
+            className="object-cover"
+            priority
+            fill
+            src={image}
+            alt="Image"
+            sizes="100%"
+          />
+        </div>
+        <p className="pl-3 font-medium truncate py-5">{name}</p>
       </div>
-      <p className="font-medium truncate py-5">{name}</p>
-      <div className="absolute transition opacity-0 rounded-full flex items-center justify-center bg-green-500 p-3 drop-shadow-md right-5 group-hover:opacity-100 hover:scale-110">
+      <div
+        onClick={!user ? onClick : handleClick}
+        className="absolute transition opacity-0 rounded-full flex items-center justify-center bg-green-500 p-3 drop-shadow-md right-5 group-hover:opacity-100 hover:scale-110">
         <FaPlay className="text-black" />
       </div>
     </button>
