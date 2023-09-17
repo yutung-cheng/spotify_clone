b6bfd51317ccdb84bf2b3a087990ae8b34ad717d
yutung-cheng <s1001913@gmail.com>
2023-09-16 20:46:18 -0400

    logic fixed

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -3,10 +3,9 @@
 import { useRouter } from "next/navigation";
 import { FaPlay } from "react-icons/fa";
 import Image from "next/image";
+
 import { useUser } from "../hooks/useUser";
 import useAuthModal from "../hooks/useAuthModal";
-import { useEffect, useState } from "react";
-import { useSessionContext } from "@supabase/auth-helpers-react";
 import { Song } from "@/types";
 import usePlayer from "../hooks/usePlayer";
 
@@ -26,45 +25,35 @@ const ListItem: React.FC<ListItemProps> = ({
   const router = useRouter();
   const { user } = useUser();
   const authModal = useAuthModal();
-  const { session } = useSessionContext();
   const player = usePlayer();
 
-  const [isClicked, setIsClicked] = useState(false);
-  const [logedIn, setLogedIn] = useState(false);
+  let isPlay = false;
 
-  const handleClick = () => player.setId(songs[0].id);
+  const handlePlay = () => {
+    isPlay = true;
+  };
 
-  const onClick = () => {
-    setIsClicked(true);
-    console.log("isClicked", isClicked);
-
-    //Add authentication before push
+  const handleRoute = () => {
     if (!user) {
-      //If not log in, trigger log in dialog.
-      authModal.onOpen();
-    } else {
-      console.log("We are here 2");
+      return authModal.onOpen();
+    }
 
-      setIsClicked(false);
+    if (isPlay) {
+      player.setId(songs[0].id);
+      isPlay = false;
+      return;
+    }
+
+    if (user && !isPlay) {
       router.push(href);
     }
   };
 
-  useEffect(() => {
-    if (session && isClicked) {
-      setLogedIn(true);
-    } else {
-      setLogedIn(false);
-    }
-    if (logedIn && isClicked) {
-      setIsClicked(false);
-      router.push(href);
-    }
-  }, [isClicked, session, logedIn, router, href]);
-
   return (
-    <button className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
-      <div onClick={onClick} className="flex flex-row">
+    <button
+      onClick={handleRoute}
+      className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
+      <div className="flex flex-row">
         <div className="relative min-h-[64px] min-w-[64px]">
           {/* This Image is imported from `next/Image` library. */}
           <Image
@@ -76,13 +65,17 @@ const ListItem: React.FC<ListItemProps> = ({
             sizes="100%"
           />
         </div>
-        <p className="pl-3 font-medium truncate py-5">{name}</p>
+        <div className="mr-30 pl-3 font-medium truncate py-5">
+          {name}
+        </div>
       </div>
-      <div
-        onClick={!user ? onClick : handleClick}
-        className="absolute transition opacity-0 rounded-full flex items-center justify-center bg-green-500 p-3 drop-shadow-md right-5 group-hover:opacity-100 hover:scale-110">
-        <FaPlay className="text-black" />
-      </div>
+      {user ? (
+        <div
+          onClick={handlePlay}
+          className="absolute transition opacity-0 rounded-full flex items-center justify-center bg-green-500 p-3 drop-shadow-md right-5 group-hover:opacity-100 hover:scale-110">
+          <FaPlay className="text-black" />
+        </div>
+      ) : null}
     </button>
   );
 };
