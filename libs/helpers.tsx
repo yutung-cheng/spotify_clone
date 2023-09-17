e21cff472ef3f00fe5f939b46e29eacdee4b7563
yutung-cheng <s1001913@gmail.com>
2023-09-17 12:52:58 -0400

    Stripe integration

---
+++ libs/helpers.tsx
@@ -0,0 +1,45 @@
+import { Price } from "@/types";
+
+export const getUrl = () => {
+  let url =
+    process.env.NEXT_PUBLIC_SITE_URL ??
+    process.env.NEXT_PUBLIC_VERCEL_URL ??
+    "http://localhost:3000/";
+
+  url = url.includes("http") ? url : `https://${url}`;
+
+  url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+
+  return url;
+};
+
+const postData = async ({
+  url,
+  data,
+}: {
+  url: string;
+  data?: {
+    price: Price;
+  };
+}) => {
+  const res: Response = await fetch(url, {
+    method: "POST",
+    headers: new Headers({ "Content-Type": "application/json" }),
+    credentials: "same-origin",
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) {
+    console.log("Error in postData", { url, data, res });
+
+    throw Error(res.statusText);
+  }
+
+  return res.json();
+};
+
+export const toDateTime = (secs: number) => {
+  var t = new Date("1970-01-01T00:30:00Z"); // Unix epoch start.
+  t.setSeconds(secs);
+  return t;
+};
