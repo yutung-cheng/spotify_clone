9bb3bb8446dc2a2916db1da4536d43c14fec07e9
yutung-cheng <s1001913@gmail.com>
2023-09-21 22:39:20 -0400

    bug fixed

--- app/api/create-checkout-session/route.tsx
+++ app/api/create-checkout-session/route.tsx
@@ -3,7 +3,7 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 import { stripe } from "@/libs/stripe";
-import { getUrl } from "@/libs/helpers";
+import { getURL } from "@/libs/helpers";
 import { createOrRetrieveCustomer } from "@/libs/supabaseAdmin";
 
 export async function POST(request: Request) {
@@ -37,8 +37,8 @@ export async function POST(request: Request) {
       subscription_data: {
         metadata,
       },
-      success_url: `${getUrl()}/account`,
-      cancel_url: `${getUrl()}/`,
+      success_url: `${getURL()}/account`,
+      cancel_url: `${getURL()}/`,
     });
 
     return NextResponse.json({ sessionId: session.id });
--- libs/helpers.tsx
+++ libs/helpers.tsx
@@ -1,15 +1,14 @@
 import { Price } from "@/types";
 
-export const getUrl = () => {
+export const getURL = () => {
   let url =
-    process.env.NEXT_PUBLIC_SITE_URL ??
-    process.env.NEXT_PUBLIC_VERCEL_URL ??
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
     "http://localhost:3000/";
-
+  // Make sure to include `https://` when not localhost.
   url = url.includes("http") ? url : `https://${url}`;
-
+  // Make sure to including trailing `/`.
   url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
-
   return url;
 };
 
@@ -18,10 +17,10 @@ export const postData = async ({
   data,
 }: {
   url: string;
-  data?: {
-    price: Price;
-  };
+  data?: { price: Price };
 }) => {
+  console.log("posting,", url, data);
+
   const res: Response = await fetch(url, {
     method: "POST",
     headers: new Headers({ "Content-Type": "application/json" }),
